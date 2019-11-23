27cc97adea62450ad68ba42d8cc058314dafeb41
Mohammad Abdul Hadi <hadi@alumni.ubc.ca>
2019-11-22 16:37:58 -0800

    Updated to handle some errors
    Handles the following error:
    
    "(node:1568) UnhandledPromiseRejectionWarning: TypeError: Cannot read property '4' of null
        at prop (C:\Users\mashuk\Downloads\Scraper\node_modules\ramda\dist\ramda.js:3399:19)
        at C:\Users\mashuk\Downloads\Scraper\node_modules\ramda\dist\ramda.js:466:28
        at f1 (C:\Users\mashuk\Downloads\Scraper\node_modules\ramda\dist\ramda.js:446:27)
        at _map (C:\Users\mashuk\Downloads\Scraper\node_modules\ramda\dist\ramda.js:262:27)
        at map (C:\Users\mashuk\Downloads\Scraper\node_modules\ramda\dist\ramda.js:5959:20)
        at C:\Users\mashuk\Downloads\Scraper\node_modules\ramda\dist\ramda.js:586:23
        at f2 (C:\Users\mashuk\Downloads\Scraper\node_modules\ramda\dist\ramda.js:473:22)
        at pluck (C:\Users\mashuk\Downloads\Scraper\node_modules\ramda\dist\ramda.js:6121:16)
        at C:\Users\mashuk\Downloads\Scraper\node_modules\ramda\dist\ramda.js:466:28
        at f1 (C:\Users\mashuk\Downloads\Scraper\node_modules\ramda\dist\ramda.js:446:27)
    (node:1568) UnhandledPromiseRejectionWarning: Unhandled promise rejection. This error originated either by throwing inside of an async function without a catch block, or by rejecting a promise which was not handled with .catch(). (rejection id: 1)
    (node:1568) [DEP0018] DeprecationWarning: Unhandled promise rejections are deprecated. In the future, promise rejections that are not handled will terminate the Node.js process with a non-zero exit code."

--- myIndex.js
+++ myIndex.js
@@ -1,46 +1,46 @@
 var gplay = require('google-play-scraper');
 const winston= require('winston');
-var num=20;
 
 logger = winston.createLogger({
     transports: [
         new winston.transports.Console(),
-        new winston.transports.File({ filename: 'logFile'})
+        new winston.transports.File({ filename: 'ReviewsCollected.txt'})
     ]
 });
 
 var myStringArray = ['APPLICATION','ANDROID_WEAR','ART_AND_DESIGN','AUTO_AND_VEHICLES','BEAUTY','BOOKS_AND_REFERENCE','BUSINESS','COMICS','COMMUNICATION','DATING','EDUCATION','ENTERTAINMENT','EVENTS','FINANCE','FOOD_AND_DRINK','HEALTH_AND_FITNESS','HOUSE_AND_HOME','LIBRARIES_AND_DEMO','LIFESTYLE','MAPS_AND_NAVIGATION','MEDICAL','MUSIC_AND_AUDIO','NEWS_AND_MAGAZINES','PARENTING','PERSONALIZATION','PHOTOGRAPHY','PRODUCTIVITY','SHOPPING','SOCIAL','SPORTS','TOOLS','TRAVEL_AND_LOCAL','VIDEO_PLAYERS','WEATHER','GAME','GAME_ACTION','GAME_ADVENTURE','GAME_ARCADE','GAME_BOARD','GAME_CARD','GAME_CASINO','GAME_CASUAL','GAME_EDUCATIONAL','GAME_MUSIC','GAME_PUZZLE','GAME_RACING','GAME_ROLE_PLAYING','GAME_SIMULATION','GAME_SPORTS','GAME_STRATEGY','GAME_TRIVIA','GAME_WORD','FAMILY','FAMILY_ACTION','FAMILY_BRAINGAMES','FAMILY_CREATE','FAMILY_EDUCATION','FAMILY_MUSICVIDEO','FAMILY_PRETEND'];
 var arrayLength = myStringArray.length;
 
-//following array of 'appId' is the output of python code. They were collected from 58 different appCategories in Google-play-store
-var myStringArray2 = require("./appIdArr");
-
-var arrayLength2 = myStringArray2.length;
-
-var assocArray = new Object();
-
-for(var i=0; i<arrayLength2; i++){ // arrayLength2
-    gplay.app({appId: myStringArray2[i], throttle: 0.5 }) // 'com.tocaboca.tocakitchen2'
-        .then((result) => {
-            assocArray[result.appId] = result.reviews;
-        });
+for(var i=0; i<arrayLength; i++){
+    gplay.list({
+        category: myStringArray[i],
+        collection: gplay.collection.TOP_FREE,
+        num: 120,
+        throttle: 0.2
+    })
+    .catch(err => console.log("Error"+err))
+    .then((resDetCat, errApps)=>{ //extract AppId from Details of Categories
+        for(var j=0; j<resDetCat.length; j++){
+            // console.log(resApps[j].appId);
+            if(resDetCat[j]!=null){
+                gplay.app({ appId: resDetCat[j].appId,
+                            throttle:0.2
+                })
+                .catch(err2 => console.log("Error2"+err2)) // Most frequent error
+                .then((resDetApps, errRevs) => { //extract Review from Details of Apps
+                        if(resDetApps != null){
+                            // console.log(resDetApps.appId, resDetApps.title, resDetApps.reviews);
+                            gplay.reviews({
+                                appId: resDetApps.appId,
+                                sort: gplay.sort.NEWEST,
+                                num: resDetApps.reviews * 2,
+                                throttle: 0.2
+                            })
+                            .catch(err3 => console.log("Error3"+err3))
+                            .then(logger.info, console.log);
+                        }
+                });
+            }
+        }
+    });
 }
-
-var delayInMilliseconds = 10*1000; //10 second
-
-setTimeout(function() {
-  //your code to be executed after 10 second
-  console.log(assocArray);
-
-  var keys = Object.keys(assocArray);
-
-  for(var i=0; i<keys.length; i++){ 
-    gplay.reviews({
-        appId: keys[i], // 'com.tocaboca.tocakitchen2'
-        sort: gplay.sort.NEWEST,
-        num: assocArray[keys[i]],
-        throttle: 8
-        })
-        .then(logger.info, logger.info);
-  }
-}, delayInMilliseconds);
